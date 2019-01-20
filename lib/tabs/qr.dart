d245a69e684c193f33c3e5a81dd7a8d1a093d02d
Alberto Caro Navarro <albertcaronava@gmail.com>
2019-01-20 01:36:15 -0600

    Añadidos todos los archivos que se llevan hasta el momento. La app ya está bastante aventajada.

--- lib/tabs/qr.dart
+++ lib/tabs/qr.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 class QRTab extends StatefulWidget {
 
@@ -7,13 +9,29 @@ class QRTab extends StatefulWidget {
 }
 
 class QRState extends State<QRTab> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
 
   @override
   Widget build(BuildContext context) {
     
-    return Center(
-      child: Text("QR"),
+    return QRView(
+      key: qrKey,
+      onQRViewCreated: _onQRViewCreated,
     );
+  }
+
+  void _onQRViewCreated(QRViewController controller){
+    var channel = controller.channel;
+    controller.init(qrKey);
+    channel.setMethodCallHandler((MethodCall call) async {
+      switch (call.method) {
+        case "onRecognizeQR":
+          dynamic arguments = call.arguments;
+          setState(() {
+            print(arguments.toString());
+          });
+      }
+    });
   }
 
 }
